7df32f703555e0fd9a3be0e39d27a42e25a8828b
DharunKumar27 <52065228+DharunKumar27@users.noreply.github.com>
2019-07-06 09:42:59 +0530

    Update and rename README.md to hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class hello{
+public static void main(String args[]){
+Scanner tt=new Scanner(System.in);
+int i;
+System.out.println("enter the value:");
+i=tt.nextInt();
+while(i>0)
+{
+System.out.println("HELLO");
+--i;
+}
+}
+}
